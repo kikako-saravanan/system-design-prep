f95374083b17d06c6efbbd88474eae5df8167a6e
M,Saravanan <saravanan.m@lowes.com>
2025-02-26 16:29:05 +0530

    feat: prometheus metris added

--- 1_loadBalancer/load-balancer-app/server2/server2.js
+++ 1_loadBalancer/load-balancer-app/server2/server2.js
@@ -1,6 +1,44 @@
 const express = require("express");
+const client = require('prom-client'); // Prometheus client library
+
 const app = express();
 const PORT = 3002;
+
+
+// Create a Prometheus registry
+const register = new client.Registry();
+if (!register.getSingleMetric('process_cpu_user_seconds_total')) {
+    client.collectDefaultMetrics({ register });
+}
+
+
+// Create a metric to measure HTTP request duration
+const httpRequestDuration = new client.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['method', 'route', 'status_code'],
+    buckets: [0.1, 0.3, 0.5, 1, 1.5, 2, 3] // Buckets for response time
+  });
+
+// Register the metric
+register.registerMetric(httpRequestDuration);
+
+
+
+// Middleware to measure request duration
+app.use((req, res, next) => {
+    const end = httpRequestDuration.startTimer();
+    res.on('finish', () => {
+      end({ method: req.method, route: req.url, status_code: res.statusCode });
+    });
+    next();
+  });
+  
+  // Metrics endpoint for Prometheus to scrape
+  app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  });
 
 app.get("/", (req, res) => {
     res.send("Response from Server 2");
--- README.md
+++ README.md
@@ -4,7 +4,20 @@ This repo will be single source of truth for System Design.
 # Load balancing app
 
 docker-compose up --build
-
 curl http://localhost
 
 docker logs nginx
+
+# Prometheus
+1️⃣ Restart all containers:
+
+docker-compose down
+docker-compose up --build -d
+
+2️⃣ Enter the server1 container:
+
+docker exec -it server1 /bin/sh
+
+3️⃣ Inside the container, check if the /metrics endpoint is running:
+
+curl http://localhost:3001/metrics
